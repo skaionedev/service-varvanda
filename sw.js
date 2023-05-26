49cc8ddffe81593ba15fb02e269fe5ceb2517286
skaionedev <skaionedev@users.noreply.github.com>
2023-05-26 11:08:02 +0000

    deploy: 4222b1d5906bd56dbf38e233d1365e8e2f9745d4

--- sw.js
+++ sw.js
@@ -3,20 +3,18 @@ self.addEventListener('install', () => {
 })
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return event.respondWith(event.request)
-  if (event.request.url.includes('.ico')) return event.respondWith(event.request)
-  if (event.request.url.includes('.jsx')) return event.respondWith(event.request)
-  if (event.request.url.includes('react')) return event.respondWith(event.request)
-  if (event.request.url.includes('vite')) return event.respondWith(event.request)
-  if (event.request.url.includes('.js')) return event.respondWith(event.request)
-
   const url = new URL(event.request.url)
-  url.hostname = 'varvanda.ru'
-  url.protocol = 'https'
+
+  if (event.request.url.includes('varvanda.ru')) {
+    url.hostname = 'varvanda.ru'
+    url.protocol = 'https'
+  }
 
   const request = new Request(url, event.request)
   console.log(request)
-  event.respondWith(fetch(request, {
-    mode: 'no-cors'
-  }))
+  event.respondWith(
+    fetch(request, {
+      mode: 'no-cors'
+    })
+  )
 })
