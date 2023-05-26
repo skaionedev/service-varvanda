3e315ffc3a5c1b5943cfca998e2d596d63d45f30
Batyr Khodzhakuliev <skaionedev@gmail.com>
2023-05-26 14:50:25 +0400

    react + router added

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import {
+  RouterProvider,
+  createBrowserRouter,
+  useLocation
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '*',
+    element: <HelloWorld />
+  }
+])
+export const App = () => {
+  return <RouterProvider router={router} />
+}
+
+function HelloWorld() {
+  const location = useLocation()
+
+  const triggerFetch = async () => {
+    await fetch(location.pathname)
+  }
+
+  useEffect(() => {
+    triggerFetch()
+  }, [location])
+
+  return <>Hello World</>
+}
